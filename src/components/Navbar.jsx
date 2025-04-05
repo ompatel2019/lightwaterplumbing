160162202711d473e462343de4bd36ada6567e44
Om Patel <ompatel2019@gmail.com>
2025-04-05 22:51:56 +1100

    bug-fix #2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -45,7 +45,7 @@ export default function Navbar() {
       {!scrolled && !isMenuOpen && (
         <header className="responsivePad bg-primary font-supreme-medium flex justify-between 2xl:py-6 lg:py-4 py-2 text-textWhite">
           <div className="flex items-center gap-2">
-            <TbHours24 className="h7" />
+            <TbHours24 className="w-5 h-5" />
             <p className="text-center p">Open 24-Hours</p>
           </div>
           <div className="flex items-center gap-2 md:rounded-lg rounded-md hover:underline transition-all">
